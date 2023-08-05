45cfba18004ef29c8e6cde056e5cadd64237942e
ranss@rambler.ru <ranss@rambler.ru>
2023-08-05 14:28:53 +0300

    init commit

---
+++ src/test/java/com/example/HomeWork31/HomeWork31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeWork31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
